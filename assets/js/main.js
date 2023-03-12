f66aaccc47e55bf72f0c5bd00c0893e007fee09c
Tung4 <tung.phingoc.ncc@gmail.com>
2023-03-12 07:00:19 +0700

    commit 3 click vào box => box1 => box2, box2 => box3, box3 => box1 + Up

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function handleColorChange() {
+  var boxList = document.querySelectorAll(".box");
+
+  var tmp = boxList[2].style.backgroundColor;
+  boxList[2].style.backgroundColor = boxList[1].style.backgroundColor;
+  boxList[1].style.backgroundColor = boxList[0].style.backgroundColor;
+  boxList[0].style.backgroundColor = tmp;
+}
